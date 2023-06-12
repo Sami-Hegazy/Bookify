b7c850ce028f9e150ea76c21d6c6b40ac4e41366
SaMi <32411864+Sami-Hegazy@users.noreply.github.com>
2023-06-12 14:33:44 +0300

    Add Animation to image

--- lib/Features/Splash/presentation/views/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/splash_view_body.dart
@@ -1,18 +1,63 @@
-import 'package:bookify/core/constants.dart';
+import 'dart:async';
+import 'dart:math';
+
 import 'package:bookify/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController rotationAnimationController;
+
+  late Animation<double> rotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initRotationAnimation();
+  }
+
+  @override
+  void dispose() {
+    rotationAnimationController.dispose();
+    super.dispose();
+  }
+
+  void initRotationAnimation() {
+    rotationAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 3000),
+    );
+    rotationAnimation = Tween<double>(begin: 0, end: pi / 2)
+        .animate(rotationAnimationController);
+    rotationAnimationController.forward();
+    Timer(const Duration(milliseconds: 3000), () {
+      rotationAnimationController.reset();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.5,
-        height: MediaQuery.of(context).size.height * 0.75,
-        child: Image.asset(AssetsImage.kLogoImage),
-      ),
+    return AnimatedBuilder(
+      animation: rotationAnimation,
+      builder: (context, child) {
+        return RotationTransition(
+          turns: rotationAnimation,
+          child: Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: MediaQuery.of(context).size.height * 0.75,
+              child: Image.asset(AssetsImage.kLogoImage),
+            ),
+          ),
+        );
+      },
     );
   }
 }
