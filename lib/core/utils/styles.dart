98f2391b0eeb92fa8f62e7e47f199f4084452641
SaMi <32411864+Sami-Hegazy@users.noreply.github.com>
2023-06-13 11:22:59 +0300

    create style file

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const textMedium = TextStyle(
+    fontSize: 20,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+  );
+}
