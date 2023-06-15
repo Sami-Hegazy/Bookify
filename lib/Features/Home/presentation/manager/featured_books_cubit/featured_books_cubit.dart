d73e16744cb2395a119072ad99a749e9add68583
SaMi <32411864+Sami-Hegazy@users.noreply.github.com>
2023-06-15 09:49:42 +0300

    create featured box method in cubit

--- lib/Features/Home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/Home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:bookify/Features/Home/data/Repo/home_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,5 +9,16 @@ import '../../../data/models/book_model/book_model.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
