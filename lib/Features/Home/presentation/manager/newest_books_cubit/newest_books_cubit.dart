893cbf0829de246fa2809db03c7f6ee1f670fb1a
SaMi <32411864+Sami-Hegazy@users.noreply.github.com>
2023-06-15 10:00:16 +0300

    newest book cubit done

---
+++ lib/Features/Home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bookify/Features/Home/data/Repo/home_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:equatable/equatable.dart';
+
+import '../../../data/models/book_model/book_model.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  HomeRepo homeRepo;
+
+  Future<void> ferchNewestBookks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
+}
