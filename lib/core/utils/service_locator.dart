bce17e6d1abc15fef09d22fe8e34aa1d933e70a0
SaMi <32411864+Sami-Hegazy@users.noreply.github.com>
2023-06-15 10:42:33 +0300

    Setup Service Locator

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,11 @@
+import 'package:bookify/Features/Home/data/Repo/home_repo_impl.dart';
+import 'package:bookify/core/utils/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImp>(HomeRepoImp(getIt.get<ApiService>()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:bookify/Features/Home/data/Repo/home_repo_impl.dart';
+import 'package:bookify/Features/Home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookify/Features/Home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookify/core/utils/colors.dart';
+import 'package:bookify/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'core/utils/app_routes.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BookifyApp());
 }
 
@@ -13,12 +19,21 @@ class BookifyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRoutes.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: MyColors.kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImp>())),
+        BlocProvider(
+            create: (context) => NewestBooksCubit(getIt.get<HomeRepoImp>())),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRoutes.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: MyColors.kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
