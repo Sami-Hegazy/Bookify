035d769eec2dbc49eba0c8edf6744ce8687bac8f
SaMi <32411864+Sami-Hegazy@users.noreply.github.com>
2023-07-23 10:38:17 +0300

    refactor to use case class

---
+++ lib/Features/Home/domain/use cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:bookify/Features/Home/domain/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../Repo/home_repo.dart';
+import '../../../../core/use_case/no_param_use_case.dart';
+
+class FetchFeaturedBooksUseCase extends UseCase<List<BookModel>> {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookModel>>> call() async {
+    return await homeRepo.fetchFeaturedBooks();
+  }
+}
