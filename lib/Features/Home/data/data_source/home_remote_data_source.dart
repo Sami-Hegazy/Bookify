2cfef0a3c5e7611f4013f4ece88bcc64fdb9b8f8
SaMi <32411864+Sami-Hegazy@users.noreply.github.com>
2023-07-23 11:40:23 +0300

    create home remote data source

---
+++ lib/Features/Home/data/data_source/home_remote_data_source.dart
@@ -0,0 +1,18 @@
+import 'package:bookify/Features/Home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() {
+    throw UnimplementedError();
+  }
+}
--- lib/Features/Home/domain/Repo/home_repo.dart
+++ lib/Features/Home/domain/Repo/home_repo.dart
@@ -1,10 +1,11 @@
-import 'package:bookify/Features/Home/data/models/book_model/book_model.dart';
 import 'package:bookify/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 
+import '../entities/book_entity.dart';
+
 abstract class HomeRepo {
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
-  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
-  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchSimilarBooks(
       {required String category});
 }
